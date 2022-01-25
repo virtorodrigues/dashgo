40c99c2d09af1d04ce5f2ed70ffe15a76397f6f9
vitor <santos_vitao@hotmail.com>
2022-01-25 16:06:22 -0300

    add create user

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -7,6 +7,10 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { Header } from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
 import { Input } from '../../components/Form/Input';
+import { useMutation } from "react-query";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
 type CreateUserFormData = {
   name: string;
@@ -16,6 +20,21 @@ type CreateUserFormData = {
 }
 
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      }
+    })
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users')
+    }
+  });
 
   const createUserFormSchema = yup.object({
     name: yup.string().required('Nome obrigatório'),
@@ -33,8 +52,10 @@ export default function CreateUser() {
   });
   const { errors } = formState;
 
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = async () => {
-    await new Promise(resolve => setTimeout(resolve, 1000));
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+    await createUser.mutateAsync(values);
+
+    router.push('/users');
   }
 
   return (
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
   Tr,
   useBreakpointValue,
 } from "@chakra-ui/react";
+import { GetServerSideProps } from "next";
 
 import Link from "next/link";
 import { useState } from "react";
@@ -25,12 +26,15 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
 import { api } from "../../services/api";
-import { useUsers } from "../../services/hooks/useUsers";
+import { getUsers, useUsers } from "../../services/hooks/useUsers";
 import { queryClient } from "../../services/queryClient";
 
+//export default function UserList({ users }) {
 export default function UserList() {
   const [page, setPage] = useState(1);
-  const { data, isLoading, error, isFetching, refetch } = useUsers(page);
+  const { data, isLoading, error, isFetching, refetch } = useUsers(page, {
+    //initialData: users
+  });
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -161,3 +165,15 @@ export default function UserList() {
     </Box >
   )
 }
+/*
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  //const { users, totalCount } = await getUsers(1);
+
+  return {
+    props: {
+      // users,
+      // totalCount,
+    }
+  }
+}*/
